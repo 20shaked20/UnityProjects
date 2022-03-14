e07a9d2c450e07961d027f37b4175563e30f34b8
Shaked <shakedlevi32@gmail.com>
2022-03-14 19:31:29 +0200

    first commit

---
+++ Player.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if space key is pressed down.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+}
