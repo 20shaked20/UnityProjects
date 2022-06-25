a401bbeee1e7e8781cc764d2e003847925e2a58b
Shaked Levi <shakedlevi32@gmail.com>
2022-06-25 20:58:37 +0300

    minor changes, now can pick up 3d items and show them

--- GroundItem.cs
+++ GroundItem.cs
@@ -1,8 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class GroundItem : MonoBehaviour
+public class GroundItem : MonoBehaviour, ISerializationCallbackReceiver
 {
     public ItemObject item;
+
+    public void OnAfterDeserialize()
+    {
+    }
+
+    public void OnBeforeSerialize()
+    {   
+        /*2d*/
+        // GetComponentInChildren<SpriteRenderer>().sprite = item.uiDisplay;
+        // EditorUtility.SetDirty(GetComponentInChildren<SpriteRenderer>());
+
+        /*3d*/
+        GetComponentInChildren<MeshFilter>().mesh = item.groundItem.GetComponent<MeshFilter>().sharedMesh;
+        EditorUtility.SetDirty(GetComponentInChildren<MeshFilter>());
+        GetComponentInChildren<MeshRenderer>().material = item.groundItem.GetComponent<MeshRenderer>().sharedMaterial;
+        EditorUtility.SetDirty(GetComponentInChildren<MeshRenderer>());
+    }
 }
